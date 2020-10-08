c80adda477fb435a6d51aa32fba3dfc7203bc200
Hela Souissi <hela.souissi@talan.com>
2020-10-08 18:11:23 +0200

    project structure

---
+++ ManyToManyDemo/src/main/java/com/exemple/manytomany/ManyToManyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.exemple.manytomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyToManyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManyToManyDemoApplication.class, args);
+	}
+
+}
---
+++ ManyToManyDemo/src/test/java/com/exemple/manytomany/ManyToManyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemple.manytomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManyToManyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
